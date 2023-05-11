29b556d0b9f465de49632c6b9b692b0a2784fa6b
Edson <edson_a@outlook.com>
2023-05-11 20:36:39 -0300

    aula-23

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-
+import Carousel from './components/Carousel';
 import './App.css';
+import movies from './mock/movies.mock';
 
 function App() {
   return (
     <>
       <Hero score={10} />
       <Navbar />
+      <Carousel title='Filmes' data={movies} />
     </>
   )
 }
